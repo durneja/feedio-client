c903dad15f9efec8462d985371e93564a6068353
durneja <kinshuk89@gmail.com>
2022-05-23 15:11:23 +0530

    Updating URL

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -19,7 +19,7 @@ export default function Dashboard(props) {
   useEffect(() => {
     // trigger API call here on page load
 
-    const rpcClient = new rpc.RPCClient("http://seed1t4.neo.org:20332");
+    const rpcClient = new rpc.RPCClient("https://testnet1.neo.coz.io:443");
     const feedioScriptHash = "e0bd649469db432189f15cf9edbe5b1b8bd20a5f";
 
     const fetchPrice = async (token) => {
